b2177cc3aba3d5727e2c9b17027aa431b15178cb
glkru <gl.krutoi@mail.ru>
2025-02-21 10:43:09 +0300

    добавил еще один кастомный эвентхендлер

---
+++ ChessUI/App/Model/MakeMoveEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using ChessLogic;
+using ChessLogic.Boardik;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessUI.App.Model
+{
+    class MakeMoveEventArgs:EventArgs
+    {
+        public Board_Base Board { get; }
+        public Player Player { get; }
+
+        MakeMoveEventArgs(Board_Base Board, Player Player)
+        {
+            this.Board = Board;
+            this.Player = Player;
+        }
+    }
+}
