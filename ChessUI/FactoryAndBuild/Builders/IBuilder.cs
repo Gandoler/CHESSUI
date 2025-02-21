23c4e9c91a79bdaa0325f195ae46815e5da6a9d8
glkru <gl.krutoi@mail.ru>
2025-02-21 11:28:32 +0300

    вернул поля

--- ChessUI/FactoryAndBuild/Builders/IBuilder.cs
+++ ChessUI/FactoryAndBuild/Builders/IBuilder.cs
@@ -12,7 +12,20 @@ namespace ChessUI.FactoryAndBuild.Builders
 {
     interface IBuilder
     {
-        
+        public PresenterBuilder WithChessWindow(MainWindow chessWindow);
+
+
+        public PresenterBuilder WithGameState(GameState gameState);
+
+        public PresenterBuilder WithModelka(Modelka modelka);
+
+        public PresenterBuilder WithPromotionMenu(Lazy<PromotionMenu> promMenu);
+
+        public PresenterBuilder WithGameOverMenu(Lazy<GameOverMenu> lazyGameOverMenu);
+
+        public PresenterBuilder WithPauseMenu(PauseMent pauseMenu);
+
+        public PresenterBuilder WithBrush(SolidColorBrush brush);
 
         public Presenter Build();
     }
