f1588e489e704a8072719c260817f1613cc60b1e
glkru <gl.krutoi@mail.ru>
2025-02-21 11:35:49 +0300

    добавил фабрику

--- ChessUI/FactoryAndBuild/Builders/Director.cs
+++ ChessUI/FactoryAndBuild/Builders/Director.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ChessUI.Code;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,18 @@ namespace ChessUI.FactoryAndBuild.Builders
 {
     class Director
     {
+        private readonly 
+        private readonly IBuilder _builder;
+
+        public Director(IBuilder builder)
+        {
+            _builder = builder;
+        }
+
+        public Presenter Build()
+        {
+            _builder.
+
+        }
     }
 }
---
+++ ChessUI/FactoryAndBuild/Factory/IPresenterComponentsFactory.cs
@@ -0,0 +1,16 @@
+﻿using ChessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessUI.FactoryAndBuild.Factory
+{
+    interface IPresenterComponentsFactory
+    {
+        Lazy<PromotionMenu> CreatePromotionMenu(Player currentPlayer);
+        Lazy<GameOverMenu> CreateGameOverMenu(GameResult result, Player currentPlayer);
+        PauseMent CreatePauseMenu();
+    }
+}
